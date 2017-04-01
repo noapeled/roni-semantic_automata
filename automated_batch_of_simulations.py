38e7353fbc51fc5c85d8f7a613b280f7f7afd3a6
Noa Peled <noapeled@gmail.com>
2017-04-01 17:54:55 +0300

    Automation ; main

--- automated_batch_of_simulations.py
+++ automated_batch_of_simulations.py
@@ -7,18 +7,29 @@ def run_single_simulation_for_multiprocessing(args_and_kwargs):
     return run_single_simulation(*args, **kwargs)
 
 
-if __name__ == '__main__':
+def main(quantifier_type, initial_temperature, threshold, alpha,
+         num_simulations,
+         num_processes=2,
+         **kwargs):
     pool = Pool(processes=2, maxtasksperchild=1)
-    # itr = pool.imap_unordered(run_single_simulation_for_multiprocessing,
-    #                           [(('EXACTLY', 2000, 1.0, 0.95),
-    #                            dict(
-    #                                    ns=(2, 5, 9), min_sample_for_each_n=5, max_sample_for_each_n=10,
-    #                                    min_zeros_per_positive_example=0, max_zeros_per_positive_example=20))] * 2)
-    # itr = pool.imap_unordered(run_single_simulation_for_multiprocessing,
-    #                           [(('ALL', 2000, 1.0, 0.95),
-    #                            dict(min_set_size=5, max_set_size=61, number_of_pairs=50))] * 8)
     itr = pool.imap_unordered(run_single_simulation_for_multiprocessing,
-                              [(('ALL_OF_THE_EXACTLY', 2000, 1.0, 0.95),
-                               dict(ns=(2, 5, 9), min_sample_for_each_n=5, max_sample_for_each_n=10))] * 8)
+                              [((quantifier_type, initial_temperature, threshold, alpha),kwargs)] *
+                              num_simulations)
     for run_return_value in itr:
         print('Finished run, return value is:\n%s' % run_return_value)
+
+
+if __name__ == '__main__':
+    main('EXACTLY', 2000, 1.0, 0.95,
+         num_simulations=10,
+         ns=(2, 5, 9),
+         min_sample_for_each_n=5,
+         max_sample_for_each_n=10,
+         min_zeros_per_positive_example=0,
+         max_zeros_per_positive_example=20)
+    main('ALL', 2000, 1.0, 0.95,
+         num_simulations=10,
+         min_set_size=5, max_set_size=61, number_of_pairs=50)
+    main('ALL_OF_THE_EXACTLY', 2000, 1.0, 0.95,
+         num_simulations=10,
+         ns=(2, 5, 9), min_sample_for_each_n=5, max_sample_for_each_n=10)
