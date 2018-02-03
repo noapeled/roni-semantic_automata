c1a3708115c67516b9886b88cf30693f9389f6a7
Noa Peled <noapeled@gmail.com>
2018-02-03 20:47:05 +0200

    Init hyp

--- mdl_experiments.py
+++ mdl_experiments.py
@@ -33,19 +33,39 @@ def create_dfa_all():
     )
 
 
-def compute_mdl_differences(min_n, max_n):
+def create_dfa_init_hyp():
+    return DFA(
+        states = ['q0', 'qF'],
+        transitions={'q0': {'0': 'q0', '1': 'q0', '#': 'qF'}},
+        initial='q0',
+        accepting=['qF']
+    )
+
+
+# def compute_mdl_differences_all_vs_all_of_the_exactly(min_n, max_n):
+#     results = {}
+#     for n1 in range(min_n, max_n + 1):
+#         for n2 in range(n1, max_n + 1):
+#             results[n1, n2] = DFA_Annealer.compare_energy(
+#                 create_dfa_all(),
+#                 create_dfa_all_of_the_exactly((n1, n2)),
+#                 sorted(['1' * n1, '1' * n2] * ARBITRARY_REPETITIONS_ALL_OF_EXACTLY)
+#             )
+#     return results
+
+def compute_mdl_differences_init_hyp_vs_all_of_the_exactly(min_n, max_n):
     results = {}
     for n1 in range(min_n, max_n + 1):
         for n2 in range(n1, max_n + 1):
             results[n1, n2] = DFA_Annealer.compare_energy(
-                create_dfa_all(),
+                create_dfa_init_hyp(),
                 create_dfa_all_of_the_exactly((n1, n2)),
                 sorted(['1' * n1, '1' * n2] * ARBITRARY_REPETITIONS_ALL_OF_EXACTLY)
             )
     return results
 
 
-def plot_mdl_differences(max_n, matrix_as_dict):
+def plot_mdl_differences(image_file_name, max_n, matrix_as_dict):
     fig, ax = plt.subplots()
     mask = np.zeros((max_n + 1, max_n + 1))
     matrix_as_array = np.zeros((max_n + 1, max_n + 1))
@@ -58,10 +78,12 @@ def plot_mdl_differences(max_n, matrix_as_dict):
         ax.invert_yaxis()
         ax.set_xlim(xmin=1)
         ax.set_ylim(ymin=1)
-        plt.savefig('mdl_experiment_1i.png')
+        plt.savefig(image_file_name)
 
 
 if __name__ == '__main__':
     # print('\n'.join(str(item) for item in sorted(compute_mdl_differences(1, 20).items(),
     #                                              key=lambda pair: pair[1])))
-    plot_mdl_differences(20, compute_mdl_differences(1, 20))
+    plot_mdl_differences(
+        'init_hyp_vs_all_of_the_exactly.png',
+        20, compute_mdl_differences_init_hyp_vs_all_of_the_exactly(1, 20))
