cc5d99b050702b8374006853f7d9cd4bc35de4f2
Noa Peled <noapeled@gmail.com>
2018-12-30 15:57:16 +0100

    s/main/run_batch

--- automated_batch_of_simulations.py
+++ automated_batch_of_simulations.py
@@ -35,25 +35,25 @@ if __name__ == '__main__':
               num_simulations=1,
               min_set_size=5, max_set_size=61, number_of_pairs=50)
 
-    # main(base_seed, 'NONE', 1500, 1.0, 0.93,
+    # run_batch(base_seed, 'NONE', 1500, 1.0, 0.93,
     #      num_simulations=10,
     #      min_set_size=5,
     #      max_set_size=61,
     #      number_of_pairs=50)
     #
-    # main(base_seed, 'BETWEEN_WITH_FIXED_UNIVERSE_SIZE', 2000, 1.0, 0.95,
+    # run_batch(base_seed, 'BETWEEN_WITH_FIXED_UNIVERSE_SIZE', 2000, 1.0, 0.95,
     #      num_simulations=10,
     #      all_ones=[],
     #      at_least_ones=3, at_most_plus_1_ones=6, fixed_universe_size=10,
     #      number_of_positive_examples=50)
     #
-    # main(base_seed, 'BETWEEN_WITH_DYNAMIC_UNIVERSE_SIZE', 2500, 1.0, 0.95,
+    # run_batch(base_seed, 'BETWEEN_WITH_DYNAMIC_UNIVERSE_SIZE', 2500, 1.0, 0.95,
     #      num_simulations=10,
     #      add_examples_which_are_all_ones_of_these_lengths=[],
     #      at_least_ones=5, at_most_ones=61, min_size_of_universe=20,
     #      max_size_of_universe=80, number_of_positive_examples=50)
     #
-    # main(base_seed, 'EXACTLY', 3700, 1.0, 0.96,
+    # run_batch(base_seed, 'EXACTLY', 3700, 1.0, 0.96,
     #      num_simulations=10,
     #      ns=(2, 5, 9),
     #      min_sample_for_each_n=5,
@@ -61,7 +61,7 @@ if __name__ == '__main__':
     #      min_zeros_per_positive_example=0,
     #      max_zeros_per_positive_example=20)
     #
-    # main(base_seed, 'ALL_OF_THE_EXACTLY', 3800, 1.0, 0.97,
+    # run_batch(base_seed, 'ALL_OF_THE_EXACTLY', 3800, 1.0, 0.97,
     #      num_simulations=10,
     #      ns=(2, 5, 9), min_sample_for_each_n=5, max_sample_for_each_n=10)
 
