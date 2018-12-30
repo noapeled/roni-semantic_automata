829ee8700577e9e7ccada31da6ffdafc863cb63e
Noa Peled <noapeled@gmail.com>
2018-12-30 18:10:31 +0100

    Pickle without maximize, and save convergence figures

--- optimize.py
+++ optimize.py
@@ -13,30 +13,38 @@ def f_optimize_all(params, alpha, threshold, num_simulations, run_batch_kwargs):
         alpha=alpha,
         num_simulations=num_simulations,
         **run_batch_kwargs)
-    return total_success / float(num_simulations)
+    return -total_success / float(num_simulations)
 
 
-def optimize(alpha, threshold, num_simulations_in_each_batch, run_batch_kwargs):
+def optimize(initial_temperature_domain, num_iter_opt_init, num_iter_opt_run, alpha, threshold,
+             num_simulations_in_each_batch, run_batch_kwargs):
     bayes_opt = BayesianOptimization(
-        maximize=True,
+        initial_design_numdata=num_iter_opt_init,
         f=partial(f_optimize_all,
                   alpha=alpha,
                   threshold=threshold,
                   num_simulations=num_simulations_in_each_batch,
                   run_batch_kwargs=run_batch_kwargs),
         domain=[
-            dict(name='initial_temperature', type='continuous', domain=(1, 10000)),
+            dict(name='initial_temperature', type='continuous', domain=initial_temperature_domain),
         ])
-    bayes_opt.run_optimization(max_iter=15,
+    bayes_opt.run_optimization(max_iter=num_iter_opt_run,
                                verbosity=True,
                                report_file='opt_report.txt',
                                evaluations_file='opt_evaluations.txt',
                                models_file='opt_models.txt')
-    pickle.dump(bayes_opt, 'bayes_opt.pkl')
+    with open('bayes_opt.pkl', 'wb') as f_pkl:
+        pickle.dump(bayes_opt, f_pkl)
+    bayes_opt.plot_convergence('opt_convergence.png')
+    bayes_opt.plot_acquisition('opt_acquisition.png')
 
 
 if __name__ == '__main__':
-    optimize(alpha=0.96,
-             threshold=1,
-             num_simulations_in_each_batch=100,
-             run_batch_kwargs=dict(min_set_size=5, max_set_size=61, number_of_pairs=50))
+    optimize(
+        initial_temperature_domain=(1, 10000),
+        num_iter_opt_init=5,
+        num_iter_opt_run=15,
+        alpha=0.96,
+        threshold=1,
+        num_simulations_in_each_batch=1,
+        run_batch_kwargs=dict(min_set_size=5, max_set_size=61, number_of_pairs=50))
