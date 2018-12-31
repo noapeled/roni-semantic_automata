8ec4b5ff235830fd088c932cb30d83b378be2d23
Noa Peled <noapeled@gmail.com>
2018-12-31 17:44:25 +0100

    Success rate plot

--- optimize_grid.py
+++ optimize_grid.py
@@ -1,8 +1,29 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 import itertools
 import numpy as np
 from printer import set_up_logging, info
 import os
 from automated_batch_of_simulations import run_batch
+
+
+def heatmap_of_results(quantifier, results_csv_path):
+    fig, ax = plt.subplots(figsize=(12, 10))
+    plt.rcParams.update({'font.size': 16})
+    sns.heatmap(
+        ax=ax,
+        data=pd.read_csv(results_csv_path)\
+            .assign(percent_success=lambda df: -df.evaluation * 100)\
+            .pivot('alpha', 'initial_temperature', 'percent_success'),
+        linewidths=11, linecolor='white', cmap='inferno_r', cbar_kws={'format': '%.0f%%'})
+    ax.set_title('Success Rate of SA Learner for Q-Det %s' % quantifier, fontdict={'size': 26})
+    ax.set_ylabel('$\\alpha$', fontdict={'size': 28})
+    ax.set_xlabel('Initial $T$', fontdict={'size': 24})
+    ax.invert_yaxis()
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig(os.path.join(os.path.dirname(results_csv_path), 'opt_%s.png' % quantifier))
 
 
 def f_inittemp_and_alpha(alpha, initial_temperature, quantifier_type, threshold, num_simulations, run_batch_kwargs):
@@ -45,11 +66,12 @@ def optimize_inittemp_and_alpha(quantifier_type, alpha_domain, initial_temperatu
 
 if __name__ == '__main__':
     set_up_logging('out.log')
-    optimize_inittemp_and_alpha(
-        quantifier_type='ALL',
-        alpha_domain=(round(x, 2) for x in np.arange(0.8, 1.0, 0.01)),
-        initial_temperature_domain=range(500, 10000, 500),
-        threshold=1,
-        num_simulations_in_each_batch=100,
-        run_batch_kwargs=dict(min_set_size=5, max_set_size=61, number_of_pairs=50)
-    )
+    # optimize_inittemp_and_alpha(
+    #     quantifier_type='ALL',
+    #     alpha_domain=(round(x, 2) for x in np.arange(0.8, 1.0, 0.01)),
+    #     initial_temperature_domain=range(500, 10000, 500),
+    #     threshold=1,
+    #     num_simulations_in_each_batch=100,
+    #     run_batch_kwargs=dict(min_set_size=5, max_set_size=61, number_of_pairs=50)
+    # )
+    heatmap_of_results('ALL', os.path.join('opt_grid', 'opt_temperature_and_alpha', 'ALL', 'grid_eval.csv'))
